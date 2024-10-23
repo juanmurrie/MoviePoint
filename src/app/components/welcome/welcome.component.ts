32abe91ecd553f0da79748ff6712a1dbe03d48de
juan_ <murriejuan@gmail.com>
2024-10-23 16:50:01 -0300

    cambios en pagina principal y cree la topbar

---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+  constructor(private router: Router){}
+
+  goToLogIn(){
+    this.router.navigate(['/LogIn']);
+  }
+
+  goToRegister(){
+    this.router.navigate(['/Register'])
+  }
+}
