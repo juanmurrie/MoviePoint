740869f866d522e60b6d67b551622a26cfa9e8c5
juan_ <murriejuan@gmail.com>
2024-11-17 18:25:49 -0300

    header y footer listos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { MovieListComponent } from '../movies/movie-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MovieListComponent],
+  imports: [MovieListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
