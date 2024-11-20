ca16b78d9f3ad646518d66841750f3dc004e52b7
cris <criscerillano@gmail.com>
2024-11-20 00:49:02 -0300

    ahora se ven la pelis(faltan populares)

--- src/app/components/movies/movie-list.component.ts
+++ src/app/components/movies/movie-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,9 +10,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent {
+  @Input('movie') movie: any;
+
+  constructor(){}
+  ngOnInit(): void { 
+    console.log(this.movie);   
+  }
+
+  getImagen() {
+    return this.movie.Poster !== 'N/A' ? this.movie.Poster : 'https://via.placeholder.com/600'
+  }
+
   popularMovies = [
     { id: 1, title: 'Pelicula 1', image: 'assets/MoviePoint-logo.webp' },
-    { id: 2, title: 'Pelicula 2', image: 'assets/MoviePoint-logo.png' },
-    { id: 3, title: 'Pelicula 3', image: 'assets/MoviePoint-logo.png' },
+    { id: 2, title: 'Pelicula 2', image: 'assets/MoviePoint-logo.webp' },
+    { id: 3, title: 'Pelicula 3', image: 'assets/MoviePoint-logo.webp' },
   ];
 }
